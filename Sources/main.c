aa9e5db2c2c7e02d00da13de66c406fae877649b
zshoals <zshoals@gmail.com>
2023-11-13 18:35:32 -0600

    avoid visible window reconfiguration at startup

--- Sources/main.c
+++ Sources/main.c
@@ -6,7 +6,20 @@ int kickstart(int argc, char** argv)
 {
 	//Note(zshoals):> These are basic defaults just to start Kinc. They're almost immediately overwritten by the config file,
 	//  which can only be loaded after kinc_init (unfortunately)
-	int primary_window = kinc_init("Heliotrope Fields", 800, 600, NULL, NULL);
+
+	//NOTE(<zshoals> 11-13-2023):> We use this window options dummy to avoid an intermediate window popping up
+	//	before we actually configure it. This is solely here for wo_dummy.visible = false
+	kinc_window_options_t wo_dummy = {0};
+	{
+		wo_dummy.x = -1;
+		wo_dummy.y = -1;
+		wo_dummy.width = 800;
+		wo_dummy.height = 600;
+		// wo_dummy.display_index = 0; pls work without initializing kinc_display :((((
+		wo_dummy.visible = false;
+	}
+
+	int primary_window = kinc_init("Heliotrope Fields", 800, 600, &(wo_dummy), NULL);
 	rl_game_loop_boot(primary_window);
 
 	return 0;
